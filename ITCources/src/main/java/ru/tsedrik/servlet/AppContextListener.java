07fed3e293eb7139585ad5da430406953df68d57
cedrikns <cedrikns@gmail.com>
2021-06-10 23:16:47 +0300

    Lesson 37 Add Logging

--- ITCources/src/main/java/ru/tsedrik/servlet/AppContextListener.java
+++ ITCources/src/main/java/ru/tsedrik/servlet/AppContextListener.java
@@ -1,15 +1,16 @@
 package ru.tsedrik.servlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-import java.util.logging.Logger;
-
 @WebListener
 public class AppContextListener implements ServletContextListener {
 
-    private Logger logger = Logger.getLogger(AppContextListener.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(AppContextListener.class.getName());
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
